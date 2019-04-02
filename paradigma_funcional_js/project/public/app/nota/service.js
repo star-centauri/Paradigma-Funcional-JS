c2957e7494b238921f569882d16b5b6222105208
star-centauri <limacosta25@hotmail.com>
2019-04-02 14:29:17 -0300

    partial function

--- paradigma_funcional_js/project/public/app/nota/service.js
+++ paradigma_funcional_js/project/public/app/nota/service.js
@@ -1,16 +1,29 @@
 import {handleStatus} from '../utils/promise-helpers.js';
+import { partialize } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
-const sumItems = code => notas => 
-		notas.flatMap(nota => nota.itens)
-				  .filter(item => item.codigo == code)
-				  .reduce((total, item) => total + item.valor, 0);
+
+const getItemsFromNotas = notas => notas.flatMap(nota => nota.itens);
+const filterItemByCode = (code, items) => items.filter(item => item.codigo == code);
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
 	listAll() {
-		return fetch(API).then(handleStatus);
+		return fetch(API)
+			.then(handleStatus)
+			.catch(err => {
+				console.log(err);
+				return Promise.reject('Não foi possível obter as notas fiscais');
+			});
 	},
 	sumItems(code) {
-		return this.listAll().then(sumItems(code));
+		const filterItems = partialize(filterItemByCode, code);
+		return this.listAll().then(notas => 
+							sumItemsValue(
+								filterItems(
+									getItemsFromNotas(notas)
+								)
+							)
+					);
 	}
 }
